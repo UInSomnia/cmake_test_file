961f5e2a8a99ef9ec63671eb94fd3f8d15610491
UInSomnia <133692821+UInSomnia@users.noreply.github.com>
2025-05-07 12:13:02 +0500

    Update main.cpp to user data input

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ int main()
 {
 	std::cout << "Hello, CMake!\n";
 	
-	const double num_0 = 4.5;
-	const double num_1 = 5.5;
+	double num_0 = 0.;
+	double num_1 = 0.;
+	std::cin >> num_0 >> num_1;
 	const double result = InSomnia::sum(num_0, num_1);
 	std::cout << "result: " << result << "\n";
 	
